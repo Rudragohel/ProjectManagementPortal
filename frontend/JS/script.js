2501da5ad58bc8e1fc02aeab0441df5062ae72be
Krunal Chhagani <kunalthakkar964@gmail.com>
2023-11-26 16:15:29 +0530

    modified dashboard, added pages for viewing projects , student and faculties and added toggling side bar fuctionality

---
+++ frontend/JS/script.js
@@ -0,0 +1,5 @@
+const sidebarToggle = document.querySelector("#sidebar-toggle");
+
+sidebarToggle.addEventListener("click",function(){
+    document.querySelector("#sidebar").classList.toggle("collapsed")
+})
